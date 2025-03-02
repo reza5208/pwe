9fd0b1202edd6990cd068719a4015ffb5f16a691
reza5208 <northman.reza@gmail.com>
2025-03-02 14:34:24 +0800

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,13 @@
+// Constants
+const defaultTrips = [
+    "KLIA Cargo", "MBG KLIA2", "MBG 163", "MBG AEON Maluri", "MBG NU Sentral",
+    "MBG DPulze", "MBG Setapak Sentral", "MBG Selayang", "MBG Nil ai", "MBG Redtick",
+    "MBG AEON Shah Alam", "MBG IOI Putrajaya", "MBG MRT", "MBG Pavilion Bukit Jalil",
+    "MBG Ampang", "MBG Bangsar", "MBG Setia Alam", "MBG Kota Damansara"
+];
+
+// Month names for localization
+const monthNames = [
+    "Januari", "Februari", "Mac", "April", "Mei", "Jun",
+    "Julai", "Ogos", "September", "Oktober", "November", "Disember"
+];
